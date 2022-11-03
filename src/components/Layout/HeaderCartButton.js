7065b2c21a181bb64e4356299df4a0d981084c41
Saurav Thapa <thapasaurav1996@gmail.com>
2022-11-04 00:36:45 +0530

    making items removable

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -4,7 +4,8 @@ import classes from "./HeaderCartButton.module.css";
 import CartContext from "../../store/cart-context";
 const HeaderCartButton = (props) => {
   const cartCtx = useContext(CartContext);
-  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+  const { items } = cartCtx;
+  const numberOfCartItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
   return (
